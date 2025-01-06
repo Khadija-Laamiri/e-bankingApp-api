066c713de3c7895b1682dfa7b4ddc13a01cba503
Enami Fatima Zahrae <enamifatimazahrae@gmail.com>
2025-01-06 17:41:40 +0100

    supprimer les autres microservices

--- facture-service/src/main/java/com/example/facture_service/repositories/OperatorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.facture_service.repositories;
-
-import com.example.facture_service.entities.Operator;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OperatorRepository extends JpaRepository<Operator, Long> {
-}
