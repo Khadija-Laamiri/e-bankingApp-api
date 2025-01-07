25655f4b5c2e606780ae3e0eddc86cabc53dbc59
Jenkins <jenkins@example.com>
2025-01-07 16:04:36 +0100

    update route config

--- api-gateway/src/main/java/com/example/apigateway/config/RouteConfig.java
+++ api-gateway/src/main/java/com/example/apigateway/config/RouteConfig.java
@@ -12,12 +12,24 @@ public class RouteConfig {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(r -> r.path("/api/factures/**")
+                .route(r -> r.path("/api/operators/**")
                         .uri("lb://facture-service"))
+                .route(r -> r.path("/api/agences/**")
+                        .uri("lb://user-service"))
+                .route(r -> r.path("/auth/**")
+                        .uri("lb://user-service"))
+                .route(r -> r.path("/api/sms/**")
+                        .uri("lb://user-service"))
                 .route(r -> r.path("/api/users/**")
                         .uri("lb://user-service"))
-                .route(r -> r.path("/api/payment-transaction/**")
+                .route(r -> r.path("/comptes/**")
                         .uri("lb://service-payment-transaction"))
+                .route(r -> r.path("/paiements/**")
+                        .uri("lb://service-payment-transaction"))
+                .route(r -> r.path("/transactions/**")
+                        .uri("lb://service-payment-transaction"))
+                .route(r -> r.path("/api/virtual-cards/**")
+                        .uri("lb://virtual-cards-service"))
                 .build();
     }
 
